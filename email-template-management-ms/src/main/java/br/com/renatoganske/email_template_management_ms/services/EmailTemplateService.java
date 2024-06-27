bb942a181b2236eca66505190bfb044de2a87de0
Renato Ganske Junior <renatoganske.jr@gmail.com>
2024-06-27 00:43:24 -0300

    Implementing logs at EmailTemplateService.

--- email-template-management-ms/src/main/java/br/com/renatoganske/email_template_management_ms/services/EmailTemplateService.java
+++ email-template-management-ms/src/main/java/br/com/renatoganske/email_template_management_ms/services/EmailTemplateService.java
@@ -19,38 +19,47 @@ public class EmailTemplateService {
     private final EmailTemplateRepository emailTemplateRepository;
 
     public List<EmailTemplateDto> getAll() {
+        log.info("Getting all email templates");
         return emailTemplateRepository.findAll().stream().map(EmailTemplate::toDto).toList();
     }
 
     public EmailTemplateDto getById(UUID id) {
+        log.info("Getting email template with id {}", id);
         Optional<EmailTemplate> emailTemplate = getEmailTemplate(id);
         if (emailTemplate.isPresent()) {
             return emailTemplate.get().toDto();
         }
-
-            throw new RuntimeException("Email template not found with id " + id);
+        log.error("Email template not found with id {}", id);
+        throw new RuntimeException("Email template not found with id " + id);
     }
 
     public EmailTemplate save(EmailTemplateDto emailTemplateDto) {
+        log.info("Saving email template {}", emailTemplateDto.name());
         return emailTemplateRepository.save(emailTemplateDto.toEntity());
     }
 
     public EmailTemplate update(UUID id, EmailTemplateDto updatingEmailTemplateDto) {
+        log.info("Updating email template with id {}", id);
         Optional<EmailTemplate> emailTemplate = getEmailTemplate(id);
         if (emailTemplate.isPresent()) {
             EmailTemplate template = emailTemplate.get();
             template.setName(updatingEmailTemplateDto.name());
             template.setContent(updatingEmailTemplateDto.content());
+            log.info("Email template with id {} updated", id);
             return emailTemplateRepository.save(template);
         } else {
+            log.error("Email template not found with id {}", id);
             throw new RuntimeException("Template not found with id " + id);
         }
     }
 
     public void delete(UUID id) {
-        if (emailTemplateRepository.existsById(id)) {
+        log.info("Deleting email template with id {}", id);
+        if (getEmailTemplate(id).isPresent()) {
+            log.info("Email template with id {} deleted", id);
             emailTemplateRepository.deleteById(id);
         } else {
+            log.error("Email template not found with id {}", id);
             throw new RuntimeException("Template not found with id " + id);
         }
     }
